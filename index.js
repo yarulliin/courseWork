7b248c91b6b4f1d623a0a3b1a98840e728ac1144
Rifat Yarullin <rifat.jarullin@gmail.com>
2021-02-17 21:48:56 +0300

    added filters

--- index.js
+++ index.js
@@ -2,27 +2,29 @@ const toMax = document.getElementById('toMax');
 const toMin = document.getElementById('toMin');
 const filter = document.getElementById('filter_logo');
 const svg = document.getElementsByClassName('svg_2');
-const account_btns = document.getElementsByClassName('acc_text');
+const checkboxex = document.getElementsByClassName('checkbox');
+const priceFrom = document.getElementById('from');
+const priceTo = document.getElementById('to');
 const wrapperMenu = document.querySelector('.wrapper-menu');
 const wrapper = document.querySelector('.input-wrapper');
 const textInput = document.querySelector("input[type='text']");
 const headerMenu = document.querySelector('.header_menu');
 const filterMenu = document.querySelector('.menu');
 const filterBlock = document.querySelector('.filter');
-
-Array.from(account_btns, el => {
-  el.addEventListener('click', () => {
-    if(getComputedStyle(el).fontWeight == 200) {
-      el.style.fontWeight = 700;
-    }
-    else el.style.fontWeight = 200;
-  })
-})
+const reset = document.querySelector('.reset_params');
 
 Array.from(svg, el => {
   el.addEventListener('click', () => {
     el.classList.toggle('filled');
   })
+})
+
+reset.addEventListener('click', () => {
+  Array.from(checkboxex, el => {
+    el.querySelector('input').checked = false;
+  })
+  priceFrom.value = '';
+  priceTo.value = '';
 })
         
 textInput.addEventListener('keyup', event => {
@@ -46,7 +48,6 @@ toMax.addEventListener('click', () => {
         toMax.style.fontWeight = 700;
         toMin.style.fontWeight = 200;
     }
-    else toMax.style.fontWeight = 200;
 })
 
 toMin.addEventListener('click', () => {
@@ -54,5 +55,4 @@ toMin.addEventListener('click', () => {
         toMin.style.fontWeight = 700;
         toMax.style.fontWeight = 200;
     }
-    else toMin.style.fontWeight = 200;
 })
